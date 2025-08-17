07c3f1e640f9bbf818267deb85109605ac18513e
327050@wu.com <Tanuj.Choudhary@westernunion.com>
2025-08-18 02:43:33 +0530

    Additional Change

--- app/u/[portfolioUrl]/layout.tsx
+++ app/u/[portfolioUrl]/layout.tsx
@@ -1,8 +1,9 @@
 import Navbar from "@/components/base/Navbar";
 import ClientPortfolioProvider from "./ClientPortfolioProvider";
+import { use } from "react";
 
-export default function Layout({ children, params }: { children: React.ReactNode, params: { portfolioUrl: string } }) {
-    const { portfolioUrl } = params;
+export default function Layout({ children, params }: { children: React.ReactNode, params: Promise<{ portfolioUrl: string }> }) {
+    const { portfolioUrl } = use(params);
 
     const navItems = [
         { href: `/u/${portfolioUrl}`, label: "Home" },
