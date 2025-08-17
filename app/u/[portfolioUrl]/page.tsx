393f1fd57f7d6017f84a37f02c03c74a4685a87f
327050@wu.com <Tanuj.Choudhary@westernunion.com>
2025-08-17 22:24:16 +0530

    Additional Change

--- app/u/[portfolioUrl]/page.tsx
+++ app/u/[portfolioUrl]/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 import PublicPortfolioMain from "@/components/U/PublicPortfolioMain";
 import { useGetUPortfolioQuery } from "@/store/services/uApi";
-import { use } from "react";
+import { use, Suspense } from "react";
 
 export default function Page({ params }: { params: Promise<{ portfolioUrl: string }> }) {
-    const {portfolioUrl} = use(params);
+    const { portfolioUrl } = use(params);
     const { data, isLoading, error } = useGetUPortfolioQuery(portfolioUrl);
 
     if (isLoading) return <div>Loading...</div>;
     if (error || !data?.portfolio) return <div>Portfolio not found.</div>;
 
-    return <PublicPortfolioMain portfolio={data.portfolio} />;
+    return (
+        <Suspense fallback={<div>Loading portfolio...</div>}>
+            <PublicPortfolioMain portfolio={data.portfolio} />
+        </Suspense>
+    );
 }
