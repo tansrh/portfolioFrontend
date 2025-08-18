aaa1bb8e16e1e4be5cc1cfcd9f53a6925f7fe43c
327050@wu.com <Tanuj.Choudhary@westernunion.com>
2025-08-19 04:04:17 +0530

    Additional Changes

--- store/services/api.ts
+++ store/services/api.ts
@@ -1,12 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '@/lib/apiEndpoints';
+import baseQueryWithReauth from './baseQueryWithReauth';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ 
-    baseUrl: BASE_URL,
-    credentials: 'include',
-  }),
+  // baseQuery: fetchBaseQuery({ 
+  //   baseUrl: BASE_URL,
+  //   credentials: 'include',
+  // }),
+  baseQuery: baseQueryWithReauth,
   tagTypes: ['Portfolio', 'Blog', 'U'] as const,
   endpoints: () => ({}),
 });
---
+++ store/services/baseQueryWithReauth.ts
@@ -0,0 +1,30 @@
+import { BASE_URL } from '@/lib/apiEndpoints';
+import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { addToast } from '../toast/toastSlice';
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: BASE_URL,
+    credentials: 'include', // send cookies
+});
+
+const baseQueryWithReauth = async (args: any, api: any, extraOptions: any) => {
+    let result = await baseQuery(args, api, extraOptions);
+    if (result.error && result.error.status === 401) {
+        // Try to refresh the token
+        const refreshResult: any = await baseQuery({ url: '/auth/refresh', method: "POST" }, api, extraOptions);
+        if (refreshResult?.data) {
+            result = await baseQuery(args, api, extraOptions);
+        } else {
+            // Optionally handle logout here
+            api.dispatch(addToast({
+                message: "Session expired. Please log in again.",
+                isError: false
+            }))
+            api.dispatch({ type: 'resetStore' });
+        }
+    }
+
+    return result;
+};
+
+export default baseQueryWithReauth;
